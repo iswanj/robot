ed8a05544f581dc7c6b3a3c1e0ef983e9cbab4ee
Iswan Jumat <info@iswanjumat.com>
2019-02-18 23:45:47 +0530

    Updates

---
+++ precache-manifest.5c304d4a353bfb7171460f788800421b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "25a9000e61fd4e0824dad7d06fd98abe",
+    "url": "/robot/static/media/up.25a9000e.svg"
+  },
+  {
+    "revision": "0d939441c357921feb13b08c1118350c",
+    "url": "/robot/static/media/robot.0d939441.svg"
+  },
+  {
+    "revision": "3aa5dcbccaab655636f1682bfe2345d2",
+    "url": "/robot/static/media/right.3aa5dcbc.svg"
+  },
+  {
+    "revision": "713fe29d10415a51910d79f66c21d161",
+    "url": "/robot/static/media/left.713fe29d.svg"
+  },
+  {
+    "revision": "3e9c517e9715e9e1f4fac077d193c823",
+    "url": "/robot/static/media/down.3e9c517e.svg"
+  },
+  {
+    "revision": "1f40da0e193d6af3c179",
+    "url": "/robot/static/js/runtime~main.1f40da0e.js"
+  },
+  {
+    "revision": "c742123600d065cb5996",
+    "url": "/robot/static/js/main.c7421236.chunk.js"
+  },
+  {
+    "revision": "c6cc08c8472d4d13cfd5",
+    "url": "/robot/static/js/2.c6cc08c8.chunk.js"
+  },
+  {
+    "revision": "c742123600d065cb5996",
+    "url": "/robot/static/css/main.5d00d4b9.chunk.css"
+  },
+  {
+    "revision": "09dbe4534b5b82afb427566cc81844a1",
+    "url": "/robot/index.html"
+  }
+];
